4797eb141f502718e9b490eb44f26531aa4018bb
Arp369 <vaghani.arpan@gmail.com>
2025-08-28 15:32:50 -0400

    Initial greeting message for the app added

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking application!")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "still available !!!")
+	fmt.Println("Get you tickets here to attend.")
+	fmt.Println(conferenceName)
 }
+
+// Greet our users
