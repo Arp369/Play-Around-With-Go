968f9254aaae45fb4c40c5274c52d71640cc2ca4
Arp369 <vaghani.arpan@gmail.com>
2025-08-28 17:27:00 -0400

    code update

--- booking-app/main.go
+++ booking-app/main.go
@@ -7,10 +7,17 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking application!")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "still available !!!")
+	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v still available !!! \n", conferenceTickets, remainingTickets)
 	fmt.Println("Get you tickets here to attend.")
-	fmt.Println(conferenceName)
+
+	var userName string
+	var userTickets int
+
+	userName = "Tom"
+	userTickets = 9
+
+	fmt.Printf("Success: %v bought %v tickets !\n", userName, userTickets)
 }
 
 // Greet our users
