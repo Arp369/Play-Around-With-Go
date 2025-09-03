eb278cc861f08ed2fb7dd8a0cb2faf931fb3ab10
Arp369 <vaghani.arpan@gmail.com>
2025-09-03 14:35:25 -0400

    code update

--- booking-app/main.go
+++ booking-app/main.go
@@ -12,27 +12,28 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v still available !!! \n", conferenceTickets, remainingTickets)
 	fmt.Println("Get you tickets here to attend.")
 
-	var userName string
-	var userTickets int
-	var userEmail string
+	for {
+		var userName string
+		var userTickets int
+		var userEmail string
 
-	fmt.Print("Enter your name:")
-	fmt.Scan(&userName)
-	fmt.Print("Enter your valid email:")
-	fmt.Scan(&userEmail)
-	fmt.Print("Enter the numbers of tickets you want:")
-	fmt.Scan(&userTickets)
+		fmt.Print("Enter your name:")
+		fmt.Scan(&userName)
+		fmt.Print("Enter your valid email:")
+		fmt.Scan(&userEmail)
+		fmt.Print("Enter the numbers of tickets you want:")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, userName)
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, userName)
 
-	fmt.Printf("The whole array: %v\n", bookings)
-	fmt.Printf("The first value: %v\n", bookings[0])
-	fmt.Printf("The type of aray: %T\n", bookings)
-	fmt.Printf("The length of array: %v\n", len(bookings))
+		// fmt.Printf("The first value: %v\n", bookings[0])
+		// fmt.Printf("The type of aray: %T\n", bookings)
+		fmt.Printf("The length of array: %v\n", len(bookings))
 
-	fmt.Printf("Thank you %v for booking %v tickets. \nYou will receive a confirmation message on %v shortly!!", userName, userTickets, userEmail)
-	fmt.Printf("\n%v tickets remaining for %v", remainingTickets, conferenceName)
+		fmt.Printf("Thank you %v for booking %v tickets. \nYou will receive a confirmation message on %v shortly!!", userName, userTickets, userEmail)
+		fmt.Printf("\n%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		fmt.Printf("These are our all tickets: %v\n", bookings)
+	}
+
 }
-
-// Greet our users
